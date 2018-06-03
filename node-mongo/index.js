a83d2b5ab19dee8782ba37946ecf622948166abc
Dinesh Sinha <dinesh@vidyo.com>
2018-06-02 22:40:34 -0400

    Node Callback Hell and Promise

--- node-mongo/index.js
+++ node-mongo/index.js
@@ -4,33 +4,36 @@ const dboper = require('./operations');
 
 const url = 'mongodb://localhost:27017/conFusion';
 
-MongoClient.connect(url, (err, db) => {
-
-    assert.equal(err,null);
+MongoClient.connect(url).then((db) => {
 
     console.log('Connected correctly to server');
 
-    dboper.insertDocument(db, { name: "Vadonut", description: "Test"}, "dishes", (result) =>{
+    dboper.insertDocument(db, { name: "Vadonut", description: "Test" }, "dishes")
+        .then((result) => {
 
-        console.log("After Insert:\n");
-        console.log(result.ops);
+            console.log("After Insert:\n");
+            console.log(result.ops);
 
-        dboper.findDocument(db, "dishes", (docs) =>{
-            console.log("found documents   length = " + docs.length + "\n documents = \n" + docs[0].name + ", " + docs[0].description);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("found documents   length = " + docs.length + "\n documents = " + docs[0].name + ", " + docs[0].description);
 
-            dboper.updateDocument(db,  { name: "Vadonut" }, { description: "Updated Test" },
-            "dishes", (result) =>{
-                console.log("updated document = \n" + result.result);
-                dboper.findDocument(db, "dishes", (documents) =>{
-                    console.log("found updated documents   length = " + docs.length + "\n documents = \n" + docs[0].name + ", " + docs[0].description);
-                    db.dropCollection("dishes", (result) =>{
-                        console.log("droppedcollection result: \n" + result);
-                        db.close();
-                    });
-
-                });
-
-            });
-        });
-    });
-});
+            return dboper.updateDocument(db, { name: "Vadonut" }, { description: "Updated Test" },
+                "dishes");
+        })
+        .then((result) => {
+            console.log("updated document = \n" + result.result);
+            return dboper.findDocuments(db, "dishes");
+        })
+        .then((docs) => {
+            console.log("found updated documents   length = " + docs.length + "\n documents = " + docs[0].name + ", " + docs[0].description);
+            return db.dropCollection("dishes");
+        })
+        .then((result) => {
+            console.log("droppedcollection result: \n" + result);
+            db.close();
+        })
+        .catch((err) => console.log(err));
+    })
+    .catch((err) => console.log(err));
---
+++ pull.sh
@@ -0,0 +1,15 @@
+
+#!/bin/bash
+echo "hello, $USER. I wish to list some files of yours"
+rm /Users/dinesh/bootstrap/node/conFusionServer/routes/index.js
+mv /Users/dinesh/Downloads/index.js /Users/dinesh/bootstrap/node/conFusionServer/routes/index.js
+
+rm /Users/dinesh/bootstrap/node/conFusionServer/routes/dishRouter.js
+mv /Users/dinesh/Downloads/dishRouter.js /Users/dinesh/bootstrap/node/conFusionServer/routes/dishRouter.js
+
+
+rm /Users/dinesh/bootstrap/node/conFusionServer/routes/promoRouter.js
+mv /Users/dinesh/Downloads/promoRouter.js /Users/dinesh/bootstrap/node/conFusionServer/routes/promoRouter.js
+
+rm /Users/dinesh/bootstrap/node/conFusionServer/routes/leaderRouter.js
+mv /Users/dinesh/Downloads/leaderRouter.js /Users/dinesh/bootstrap/node/conFusionServer/routes/leaderRouter.js
